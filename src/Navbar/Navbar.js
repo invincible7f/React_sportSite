01be9c717a0d60bd90c3b6ed5d5b58d5ae549cc4
invincible7f <sashasas1994@gmail.com>
2024-11-25 01:02:13 +0200

    Navbar almost done

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './Navbar.css'
-
 class Navbar extends React.Component{
 
+  constructor(props){
+
+  super(props)
+
+  this.state ={
+   
+  imageTraining:'images/1118213_dumbbell_exercise_fitness_muscle_weight_icon.png',
+  imageNutrition:'images/nutrition.png',
+  imageShedul:'images/shedul.png'
+  }
+
+  }
 
 render(){
+
+  
 
     return(
      <nav>
       <div className='LogoName'>
-       <img alt='picture'/>
+       <img  src='images/1118213_dumbbell_exercise_fitness_muscle_weight_icon2.png' alt='picture'/>
         <h2>FitPower</h2>
       
       </div>
@@ -17,12 +30,12 @@ render(){
        <div className='navMenu'>
        <ul>
 
-        <li> <img alt='picture'/> <a href='#'>Тренировки</a></li> 
-        <li> <img alt='picture'/> <a href='#'>Питание</a></li> 
-        <li> <img alt='picture'/> <a href='#'>Расписание</a></li> 
+        <li> <img src={this.state.imageTraining} alt='trainings'/> <a href='#'>Тренировки</a></li> 
+        <li> <img src={this.state.imageNutrition} alt='nutrition'/> <a href='#'>Питание</a></li> 
+        <li> <img className='shedul' src={this.state.imageShedul} alt='pict'/> <a href='#'>Расписание</a></li> 
    
        </ul>
-       <button>Войти</button>
+       <button type='submit'>Войти</button>
        </div>
      </nav>
      
